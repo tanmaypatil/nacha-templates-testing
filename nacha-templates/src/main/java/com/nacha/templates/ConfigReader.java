4b18962f1286861eb811935a7491b2a093e84784
Tanmay Patil <tany.patil77@gmail.com>
2023-06-27 14:21:21 +0530

    Changed the path as a central path.

--- nacha-templates/src/main/java/com/nacha/templates/ConfigReader.java
+++ nacha-templates/src/main/java/com/nacha/templates/ConfigReader.java
@@ -5,11 +5,10 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
-        final static String basePath = "C:\\Users\\u725561\\p2go\\poc\\nacha-templates-testing\\nacha-templates\\src\\main\\resources";
-     
-        public static Properties readConfig(String fileName) {
+        
+    public static Properties readConfig(String fileName) {
         Properties properties = new Properties();
-        String finalPath = basePath + "\\"  + fileName;
+        String finalPath = ResourcePath.basePath + "\\"  + fileName;
         try (InputStream input = new FileInputStream(finalPath)) {
             properties.load(input);     
         }
