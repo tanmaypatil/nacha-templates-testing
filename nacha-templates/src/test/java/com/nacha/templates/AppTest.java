f8404597b5a14618a9b080423f6714375f3c956d
Tanmay Patil <tany.patil77@gmail.com>
2023-06-29 17:00:39 +0530

    moved freemarker init to BeforeAll

--- nacha-templates/src/test/java/com/nacha/templates/AppTest.java
+++ nacha-templates/src/test/java/com/nacha/templates/AppTest.java
@@ -1,69 +1,68 @@
 package com.nacha.templates;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
 
 /**
  * Unit test for simple App.
  */
+@TestInstance(Lifecycle.PER_CLASS)
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
+    FreeMarker freeMarker = new FreeMarker();
+   
+    @BeforeAll
+    public void init() {
+        freeMarker.initialiseFreeMarker();
+
     }
 
     @DisplayName("Just a simple test to generate html file using freemarker.")
     @Test
     void generateHtmlFile() {
-        FreeMarker freeMarker = new FreeMarker();
-        freeMarker.initialiseFreeMarker();
+
         HashMap root = new HashMap();
         root.put("user", "Big Joe");
         Product latest = new Product();
         latest.setUrl("products/greenmouse.html");
         latest.setName("green mouse");
         root.put("latestProduct", latest);
-        // write to a file 
-        boolean result = freeMarker.processTemplateAndWriteFile("test.ftlh", root,"response2.out");
+        // write to a file
+        boolean result = freeMarker.processTemplateAndWriteFile("test.ftlh", root, "response2.out");
         Assertions.assertTrue(result);
     }
 
     @DisplayName("NACHA ppd file generation , replace effective date ")
     @Test
     void generateNachaFile() {
-        FreeMarker freeMarker = new FreeMarker();
-        freeMarker.initialiseFreeMarker();
-        HashMap<String,Object> root = new HashMap<String,Object>();
+        HashMap<String, Object> root = new HashMap<String, Object>();
         root.put("user", "Big Joe");
         NachaConfig nacha = new NachaConfig();
         nacha.setEffDate("210212");
         root.put("nachaConfig", nacha);
-        // write to a file 
-        boolean result = freeMarker.processTemplateAndWriteFile("nacha_customer_CT_PPD.ftl", root,"nacha_ct_ppd.ach");
+        // write to a file
+        boolean result = freeMarker.processTemplateAndWriteFile("nacha_customer_CT_PPD.ftl", root, "nacha_ct_ppd.ach");
         Assertions.assertTrue(result);
     }
 
-       @DisplayName("xml file generation")
+    @DisplayName("xml file generation")
     @Test
     void generateSepaFile() {
-        FreeMarker freeMarker = new FreeMarker();
-        freeMarker.initialiseFreeMarker();
-        HashMap<String,Object> root = new HashMap<String,Object>();
+
+        HashMap<String, Object> root = new HashMap<String, Object>();
         root.put("user", "Big Joe");
         SepaConfig sepa = new SepaConfig();
         sepa.setBic("DHEUDHEU");
         root.put("sepaConfig", sepa);
-        // write to a file 
-        boolean result = freeMarker.processTemplateAndWriteFile("camt029.ftl", root,"camt029.xml");
+        // write to a file
+        boolean result = freeMarker.processTemplateAndWriteFile("camt029.ftl", root, "camt029.xml");
         Assertions.assertTrue(result);
     }
 }
-
